abf27b5ba7b31a6a98562a616ffbfe9389d12bd7
Adarsh Mishra <100149126+Adarshmishraa@users.noreply.github.com>
2023-02-01 18:00:05 +0530

    Add files via upload

---
+++ src/components/Slots.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Slots() {
+  return (
+    <div>
+      <label htmlFor="accept">
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 8.30 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 9.00 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 9.30 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 10.00 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 10.30 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 11.00 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 11.30 AM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 12.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 12.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 1.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 1.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 2.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 2.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 3.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 3.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 4.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 5.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 7.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 7.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 8.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 8.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 9.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 9.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 10.00 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 10.30 PM
+        <input type="checkbox" id="accept" name="accept" value="yes"/> 11.00 PM
+    </label>
+    </div>
+  )
+}
+
+export default Slots;
